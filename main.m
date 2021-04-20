294bd224ecda1af360be5e71c3d2fedcebcc6a87
Krzysztof <kpiekorz919@protonmail.com>
2021-04-20 11:56:09 +0200

    git ignore

--- main.m
+++ main.m
@@ -1,8 +1,12 @@
 clc
 clear
 
+% parametry układu
+l = 1; % odleglosc punktu masowego od osi obrotu
+f_c_s = 0.61; % wspolczynnik tarcia statyczny 
+f_c_k = 0.47; % wspolczynnik tarcia kinematyczny
 
-x = linspace(1, 10, 10)
-t = linspace(1, 10, 10)
-
-plot(t, x)
+A = 0
+B = 0
+C = [1 0 0 0; 0 1 0 0; 0 0 0 0; 0 0 0 0]
+D = 0
