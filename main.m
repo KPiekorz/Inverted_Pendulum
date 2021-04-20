9d63316ad20494abf2e484301301b15ec125c9a2
Krzysztof <kpiekorz919@protonmail.com>
2021-04-20 15:02:19 +0200

    simulink model works

--- main.m
+++ main.m
@@ -6,7 +6,10 @@ l = 1; % odleglosc punktu masowego od osi obrotu
 f_c_s = 0.61; % wspolczynnik tarcia statyczny 
 f_c_k = 0.47; % wspolczynnik tarcia kinematyczny
 
-A = 0
-B = 0
-C = [1 0 0 0; 0 1 0 0; 0 0 0 0; 0 0 0 0]
-D = 0
+% system model definition
+A = 0;
+B = 0;
+C = [1 0 0 0; 0 1 0 0; 0 0 0 0; 0 0 0 0];
+D = 0;
+
+
