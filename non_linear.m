7dc28d7e1913c201c06ed755e70bb55d76c3a2f0
Krzysztof <kpiekorz919@protonmail.com>
2021-04-23 19:30:45 +0200

    resolve system

---
+++ non_linear.m
@@ -0,0 +1,10 @@
+clc;
+clear;
+close all;
+
+
+fi = 0.41
+m = 0.5
+M = 1
+g = 9.81
+l = 1
---
+++ solve_system.m
@@ -0,0 +1,22 @@
+clc;
+clear;
+close all;
+
+% x1 = x 
+% x2 = theta
+% x3 = dx/dt
+% x4  = dtheta/dt
+
+syms x y x3 x4 x1 x2 m M l F fi g i j
+
+% x = ddx/ddt
+% y = ddtheta/ddt
+
+% i = sin(x2)
+% j = cos(x2)
+
+equ1 = (M + m)*x == F + m*l*i*(x4)^2 - m*l*j*y - fi*x3
+
+equ2 = (4/3)*l*y == m*g*i - j*x
+
+[solx,soly] = solve(equ1, equ2)
