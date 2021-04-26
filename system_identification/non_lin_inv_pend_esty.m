7e13a3d42e071ec6589c68eb97940dc33ce43bfa
Krzysztof <kpiekorz919@protonmail.com>
2021-04-26 22:14:59 +0200

    matlab template

--- system_identification/non_lin_inv_pend_esty.m
+++ system_identification/non_lin_inv_pend_esty.m
@@ -2,3 +2,20 @@ clc;
 clear;
 close all;
 
+%% symulacja modelu rzeczywistego w simulinku
+
+
+%% prezentacja wymuszenia i odpowiedzi modelu rzeczywistego
+
+
+%% stworzenie obiektu modelu do identyfikacji parametrycznej
+
+
+%% testowanie odpowiedzi modelu przed estymacja
+
+
+%% ientyfikacja parametrów
+
+
+%% testowanie odpowiedzi modelu po estymacji
+
