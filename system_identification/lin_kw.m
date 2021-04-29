749aa20cfbe807b693f9cc9d0cdc73ac9709cf97
piotrzajac357 <38423119+piotrzajac357@users.noreply.github.com>
2021-04-29 19:33:59 +0200

    ident_method_2_v2

--- system_identification/lin_kw.m
+++ system_identification/lin_kw.m
@@ -3,12 +3,13 @@ clear all; close all;
 m = 0.12;
 M = 0.5723;
 g = 9.81;
+J = 0.0038583;
 step_value = 10;
-T = 10;
+T = 50;
 sim_step = 0.01;
 
 l = 0.01955717;
-f = 0.0027344;
+f = 0.00027344;
 
 time = sim('lin_kw_sim.slx');
 t = time;
@@ -65,6 +66,34 @@ ylabel("Pendulum angle")
 
 subplot(2, 2, 4);
 plot(t, states_ident(:,4));
+title("Real State Pendulum velocity");
+xlabel("Time t")
+ylabel("Pendulum velocity")
+
+
+
+
+figure(4);
+subplot(2, 2, 1);
+plot(t, states_ident_v2(:,1));
+title("Real State Cart position");
+xlabel("Time t")
+ylabel("Cart position")
+
+subplot(2, 2, 2);
+plot(t, states_ident_v2(:,3));
+title("Real State Cart velocity");
+xlabel("Time t")
+ylabel("Cart velocity")
+
+subplot(2, 2, 3);
+plot(t, states_ident_v2(:,2));
+title("Real State Pendulum angle");
+xlabel("Time t")
+ylabel("Pendulum angle")
+
+subplot(2, 2, 4);
+plot(t, states_ident_v2(:,4));
 title("Real State Pendulum velocity");
 xlabel("Time t")
 ylabel("Pendulum velocity")
