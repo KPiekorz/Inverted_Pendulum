96b1fe7100f014b3596047971d7cc8f0cc12ae44
Krzysztof <kpiekorz919@protonmail.com>
2021-04-29 12:15:37 +0200

    start of new summary

--- system_identification/non_lin_inv_pend_esty.m
+++ system_identification/non_lin_inv_pend_esty.m
@@ -3,38 +3,71 @@ clear;
 close all;
 
 %% symulacja modelu rzeczywistego w simulinku
-T = 100; % czas symulacji
-sim_step = 0.01; %Krok symulacji
+T = 20; % czas symulacji
+sim_step = 0.0001; %Krok symulacji
 out = sim('pendulum_dynamics.slx', T)
 
 %% prezentacja wymuszenia i odpowiedzi modelu rzeczywistego
 figure(1);
-plot(out.tout, out.u);
+plot(out.tout, out.u, "-b");
 title("Input Cart Force")
 xlabel("Time t")
 ylabel("Force")
 
 figure(2);
 subplot(2, 2, 1);
-plot(out.tout, out.states(:,1), "*r");
+plot(out.tout, out.states(:,1), "*g");
 title("Real State Cart position");
 xlabel("Time t")
 ylabel("Cart position")
 
 subplot(2, 2, 2);
-plot(out.tout, out.states(:,3), "*r");
+plot(out.tout, out.states(:,3), "*g");
 title("Real State Cart velocity");
 xlabel("Time t")
 ylabel("Cart velocity")
 
 subplot(2, 2, 3);
-plot(out.tout, out.states(:,2), ".-r");
+plot(out.tout, out.states(:,2), "*g");
 title("Real State Pendulum angle");
 xlabel("Time t")
 ylabel("Pendulum angle")
 
 subplot(2, 2, 4);
-plot(out.tout, out.states(:,4), ".-r");
+plot(out.tout, out.states(:,4), "*g");
+title("Real State Pendulum velocity");
+xlabel("Time t")
+ylabel("Pendulum velocity")
+
+%% interpolacja stanów
+
+t_inter = 1:0.001:T;
+cart_pos = interp1(out.tout,out.states(:,1), t_inter);
+cart_velo = interp1(out.tout,out.states(:,3), t_inter);
+pend_pos = interp1(out.tout,out.states(:,2), t_inter);
+pend_velo = interp1(out.tout,out.states(:,4), t_inter);
+
+figure(3);
+subplot(2, 2, 1);
+plot(t_inter, cart_pos, "-k");
+title("Real State Cart position");
+xlabel("Time t")
+ylabel("Cart position")
+
+subplot(2, 2, 2);
+plot(t_inter, cart_velo, "-k");
+title("Real State Cart velocity");
+xlabel("Time t")
+ylabel("Cart velocity")
+
+subplot(2, 2, 3);
+plot(t_inter, pend_pos, "-k");
+title("Real State Pendulum angle");
+xlabel("Time t")
+ylabel("Pendulum angle")
+
+subplot(2, 2, 4);
+plot(t_inter, pend_velo, "-k");
 title("Real State Pendulum velocity");
 xlabel("Time t")
 ylabel("Pendulum velocity")
---
+++ system_identification/trans_equ_of_motion.m
@@ -0,0 +1,21 @@
+clc;
+close all;
+
+% x1 = x 
+% x2 = theta
+% x3 = dx/dt
+% x4  = dtheta/dt
+
+syms x y x3 x4 x1 x2 m M l F fi g i j
+
+% x = ddx/ddt
+% y = ddtheta/ddt
+
+% i = sin(x2)
+% j = cos(x2)
+
+equ1 = (M + m)*x == F + (m*l*i*(x4)^2) - (m*l*j*y) - (fi*x3)
+
+equ2 = (4/3)*l*y == (g*i) - (j*x)
+
+[x, y] = solve(equ1, equ2, x, y)
